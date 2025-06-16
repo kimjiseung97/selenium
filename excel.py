27b8c1fd5db99bce88448ea67b0fc6ac7983dcf7
jiseung <dickseung23@naver.com>
2025-06-16 23:57:58 +0900

    액셀파일 샘플 작성

---
+++ excel.py
@@ -0,0 +1,20 @@
+#액셀 모듈 임포트
+import openpyxl
+
+
+
+#엑셀파일 생성
+xlsxFile = openpyxl.Workbook()
+
+#생성한 파일에서 시트 생성
+xlsxSheet = xlsxFile.active
+
+
+#시트 특정 셀에 데이터 입력
+for i in range(10):
+    xlsxSheet.cell(row = i + 1, column = 1).value = "hi"
+#find_element().text 로 찾은 값을 넣으면 됨
+
+
+#저장
+xlsxFile.save('result.xlsx')
--- selenium_test.py
+++ selenium_test.py
@@ -28,6 +28,7 @@ customOption = Options()
 browser = webdriver.Chrome(service = customService, options = customOption)
 
 
+
 URL = "https://www.naver.com/"
 
 browser.get(URL)
@@ -54,5 +55,18 @@ browser.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[2]/div[2]/div/div/d
 time.sleep(3)
 
 
+#엑셀파일 생성
+xlsxFile = openpyxl.Workbook()
 
+#생성한 파일에서 시트 생성
+xlsxSheet = xlsxFile.active
+
+
+#시트 특정 셀에 데이터 입력
+xlsxSheet.cell(row = 1, column = 1).value = "hi"
+#find_element().text 로 찾은 값을 넣으면 됨
+
+
+#저장
+xlsxFile.save('result.xlsx')
 
